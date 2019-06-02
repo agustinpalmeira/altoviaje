2884974a66f8ed652948fc23904da7acd27b8773
Nicolas Alvarez <nicolas.alvarez@fdvsolutions.com>
2019-06-02 18:43:20 -0300

    WIP: dashboard cell

---
+++ AltoViaje/AltoViaje/Features/Dashboard/DashboardCell.swift
@@ -0,0 +1,24 @@
+//
+//  DashboardCell.swift
+//  AltoViaje
+//
+//  Created by Nicolas Alvarez on 6/2/19.
+//  Copyright © 2019 AltoViaje. All rights reserved.
+//
+
+import UIKit
+
+class DashboardCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ AltoViaje/AltoViaje/Features/Dashboard/DashboardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DashboardViewController.swift
+//  AltoViaje
+//
+//  Created by Nicolas Alvarez on 6/2/19.
+//  Copyright © 2019 AltoViaje. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
