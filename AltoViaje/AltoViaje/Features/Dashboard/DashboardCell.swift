58388769bcbb334598d52b73216e66e4cc094733
Nicolas Alvarez <nicolas.alvarez@fdvsolutions.com>
2019-06-05 20:22:15 -0300

    Adding Menu to dash controller

--- AltoViaje/AltoViaje/Features/Dashboard/DashboardCell.swift
+++ AltoViaje/AltoViaje/Features/Dashboard/DashboardCell.swift
@@ -9,16 +9,21 @@
 import UIKit
 
 class DashboardCell: UITableViewCell {
+    static let cellIdentifier = "DashboardCell"
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var placeTitle: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        containerView.layer.borderColor = UIColor.black.cgColor
+        containerView.layer.cornerRadius = 10
+        containerView.layer.borderWidth = 1
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
--- AltoViaje/AltoViaje/Features/Dashboard/DashboardViewController.swift
+++ AltoViaje/AltoViaje/Features/Dashboard/DashboardViewController.swift
@@ -9,11 +9,73 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
+    @IBOutlet weak var tripsTable: UITableView!
+    @IBOutlet weak var menuView: MenuView!
+
+    let menuItems: [MenuItemModel] = [
+        MenuItemModel(image: #imageLiteral(resourceName: "planeIcon"), name: "Vuelos"),
+        MenuItemModel(image: #imageLiteral(resourceName: "lodging"), name: "Alojamiento"),
+        MenuItemModel(image: #imageLiteral(resourceName: "trip"), name: "Recorrido"),
+        MenuItemModel(image: #imageLiteral(resourceName: "suitcase"), name: "Paquetes"),
+        MenuItemModel(image: #imageLiteral(resourceName: "equipment"), name: "Equipo"),
+        MenuItemModel(image: #imageLiteral(resourceName: "transport"), name: "Transporte")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationController?.setNavigationBarHidden(true, animated: true)
+        setupTable()
+        setupMenu()
     }
+
+    private func setupMenu() {
+        menuView.setDelegate(self)
+        menuView.setDataSource(self)
+    }
+
+    private func setupTable() {
+        tripsTable.register(UINib.init(nibName: "DashboardCell", bundle: nil), forCellReuseIdentifier: DashboardCell.cellIdentifier)
+    }
+
+
+}
+
+extension DashboardViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: DashboardCell.cellIdentifier, for: indexPath)
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+
+
+}
+
+extension DashboardViewController: UICollectionViewDelegate {
+
+}
+
+extension DashboardViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let item = collectionView.dequeueReusableCell(withReuseIdentifier: MenuCell.itemIdentifier, for: indexPath) as? MenuCell else {
+            return UICollectionViewCell()
+        }
+        item.configureCell(menuItems[indexPath.row])
+        return item
+    }
+
+
+}
+
+extension DashboardViewController: UITableViewDelegate {
 
 }
