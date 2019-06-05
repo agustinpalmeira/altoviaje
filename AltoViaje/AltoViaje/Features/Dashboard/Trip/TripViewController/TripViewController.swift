79da79ccb1902521f79cf69cf01dfb15a6743a9d
Agustin Palmeira <agustin.palmeira@fdvsolutions.com>
2019-06-05 18:52:00 -0300

    TripView and TripViewController layout

---
+++ AltoViaje/AltoViaje/Features/Dashboard/Trip/TripViewController/TripViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TripViewController.swift
+//  AltoViaje
+//
+//  Created by Agustin Palmeira on 05/06/2019.
+//  Copyright © 2019 AltoViaje. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TripViewController: UIViewController {
+	@IBOutlet weak var tripView: TripView!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+}
