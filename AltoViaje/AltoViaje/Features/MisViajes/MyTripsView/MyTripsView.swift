ef824786c79c51744df2e90aed7d802aa5f04517
Agustin Palmeira <agustin.palmeira@fdvsolutions.com>
2019-06-08 19:13:46 -0300

    Table for the trips

--- AltoViaje/AltoViaje/Features/MisViajes/MyTripsView/MyTripsView.swift
+++ AltoViaje/AltoViaje/Features/MisViajes/MyTripsView/MyTripsView.swift
@@ -9,10 +9,19 @@
 import Foundation
 import UIKit
 
+protocol MyTripsViewDelegate: class {
+	func createNewTrip()
+}
+
 class MyTripsView: UIView {
-	@IBOutlet var contentView: UIView!
-	@IBOutlet weak var nextTripButton: UIButton!
-	
+
+	//MARK: - Variables
+	@IBOutlet var myTripContainerView: UIView!
+	@IBOutlet weak var tableView: UITableView!
+
+	weak var delegate: MyTripsViewDelegate?
+
+	//MARK: - Initialization
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 		commonInit()
@@ -26,8 +35,23 @@ class MyTripsView: UIView {
 	private func commonInit() {
 		let nib = UINib(nibName: "MyTripsView", bundle: nil)
 		nib.instantiate(withOwner: self, options: nil)
-		addSubview(contentView)
-		contentView.frame = self.bounds
-		contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+		addSubview(myTripContainerView)
+		myTripContainerView.frame = self.bounds
+		myTripContainerView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+		tableView.register(UINib.init(nibName: "MyTripsCell", bundle: nil), forCellReuseIdentifier: MyTripsCell.cellIdentifier)
+	}
+
+	//MARK: - Actions
+	@IBAction func newTripButtonAction(_ sender: Any) {
+		delegate?.createNewTrip()
+	}
+
+	//MARK: - Table View methods
+	func setTableDataSource(_ dataSource: UITableViewDataSource) {
+		tableView.dataSource = dataSource
+	}
+
+	func setTableViewDelegate(_ delegate: UITableViewDelegate) {
+		tableView.delegate = delegate
 	}
 }
--- AltoViaje/AltoViaje/Features/MisViajes/MyTripsViewController.swift
+++ AltoViaje/AltoViaje/Features/MisViajes/MyTripsViewController.swift
@@ -7,4 +7,55 @@
 //
 
 import Foundation
+import UIKit
 
+class MyTripsViewController: UIViewController {
+
+	//MARK: - View life cycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setSubviews()
+	}
+
+	//MARK: - Configure subviews
+	private func setSubviews() {
+		let myTripsView = MyTripsView()
+		myTripsView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+		myTripsView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+		myTripsView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+		myTripsView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+		myTripsView.delegate = self
+		myTripsView.setTableDataSource(self)
+		myTripsView.setTableViewDelegate(self)
+	}
+}
+
+//MARK: - Extensions
+
+extension MyTripsViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 8
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return tableView.dequeueReusableCell(withIdentifier: MyTripsCell.cellIdentifier, for: indexPath)
+	}
+
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return 200
+	}
+}
+
+extension MyTripsViewController: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		//TODO: Implement the view controller that should be pushed by the navigation
+		//let viewController = ...(nibName:"", bundle: nil)
+		//navigationController?.pushViewController(viewController, animated: true)
+	}
+}
+
+extension MyTripsViewController: MyTripsViewDelegate {
+	func createNewTrip() {
+		//TODO: Implement where should this button lead to.
+	}
+}
