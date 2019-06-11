77256c06375f5ba7c0a98e4be555dbeaa16a0312
Agustin Palmeira <agustin.palmeira@fdvsolutions.com>
2019-06-11 01:11:40 -0300

    Update colors & constraints

--- AltoViaje/AltoViaje/Features/MisViajes/ItineraryViewController/ItineraryView/ItineraryView.swift
+++ AltoViaje/AltoViaje/Features/MisViajes/ItineraryViewController/ItineraryView/ItineraryView.swift
@@ -35,43 +35,56 @@ class ItineraryView: UIView {
 		var activityLabel: UILabel
 		var containerView: UIView
 
+		let color = UIColor(red: 72/255, green: 135/255, blue: 223/255, alpha: 1)
+
 		for dateActivity in activities {
 			containerView = UIView()
 			addSubview(containerView)
+			containerView.backgroundColor = color
+			containerView.layer.borderColor = color.cgColor
+			containerView.layer.cornerRadius = 5.0
+			containerView.layer.borderWidth = 2.0
 			containerView.translatesAutoresizingMaskIntoConstraints = false
 
 			dateLabel = UILabel()
+			dateLabel.font = UIFont.boldSystemFont(ofSize: 20)
+			dateLabel.textColor = .white
 			containerView.addSubview(dateLabel)
 			dateLabel.translatesAutoresizingMaskIntoConstraints = false
-			dateLabel.text = dateActivity.date
-			dateLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
-			dateLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
-			dateLabel.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+			dateLabel.text = "Fecha: " + dateActivity.date
+			dateLabel.textAlignment = .center
+			dateLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 5).isActive = true
+			dateLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -5).isActive = true
+			dateLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 5).isActive = true
 			containerView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
 			containerView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
 
 			if let view = previousView {
-				containerView.topAnchor.constraint(equalTo: view.bottomAnchor, constant: 5).isActive = true
+				containerView.topAnchor.constraint(equalTo: view.bottomAnchor, constant: 25).isActive = true
 			} else {
-				containerView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
+				containerView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
 			}
 			previousView = containerView
 
 			for activity in dateActivity.activities {
 				activityLabel = UILabel()
-				activityLabel.text = activity
+				activityLabel.text = "» " + activity
+				activityLabel.font = UIFont.boldSystemFont(ofSize: 17)
+				activityLabel.textColor = .white
+				activityLabel.numberOfLines = 0
+				activityLabel.lineBreakMode = .byWordWrapping
 				containerView.addSubview(activityLabel)
 				activityLabel.translatesAutoresizingMaskIntoConstraints = false
 				activityLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10).isActive = true
-				activityLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
+				activityLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20).isActive = true
 				if let label = previousActivityLabel {
 					activityLabel.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 5).isActive = true
 				} else {
-					activityLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 5).isActive = true
+					activityLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 15).isActive = true
 				}
 				previousActivityLabel = activityLabel
 			}
-			previousActivityLabel?.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
+			previousActivityLabel?.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -10).isActive = true
 			previousActivityLabel = nil
 		}
 	}
